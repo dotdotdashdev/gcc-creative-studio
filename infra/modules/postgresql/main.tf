682cffad37c5753778158bd1568f6d6339114148
James Calhoun <james.calhoun@gmail.com>
2026-05-14 01:02:53 -0400

    enable ipv4

--- infra/modules/postgresql/main.tf
+++ infra/modules/postgresql/main.tf
@@ -32,7 +32,7 @@ resource "google_sql_database_instance" "default" {
     }
 
     ip_configuration {
-      ipv4_enabled = true # Easy connectivity from Cloud Run without VPC peering complexity
+      ipv4_enabled = false # Easy connectivity from Cloud Run without VPC peering complexity
     }
   }
   
